d7223a8e479f498dfe1847ab765c6955cc68038c
Matt Finnell <mattfinnell104@gmail.com>
2014-07-02 09:50:50 -0700

    tried different images, some documentation

--- parralax/js/script.js
+++ parralax/js/script.js
@@ -1,17 +1,41 @@
 $(document).ready(function() {
 
+	/* Driving function that calls our parallax functionalities */
 	function scrollUpdates(){
-		var parallax = $("#parallax1");
-		if(parallaxRelativity(parallax) !== -1){
-			setParallaxDelta(parallax, 2, .4); 
+		var p1 = $("#parallax1");
+		var p2 = $("#parallax2");
+
+		if(parallaxRelativity(p1) > 0 ){
+			setParallaxDelta(p1, 15, 0); 
+		}
+
+		if(parallaxRelativity(p2) > 0){
+			setParallaxDelta(p2, 2, 0); 
 		}
 	}
 
+	/* setParallaxDelta 
+	 *	Params
+	 *		parallax	DOM object meant to be edited
+	 *		velocity  	rate at which the background shifts (via percentage);
+	 *		offset		vertical image shift		
+	 *
+	 * will set the background shift respectively to the parallaxRelativity. 
+	 * essentially sets the CSS to do what we want. 
+	 */
 	function setParallaxDelta(parallax, velocity, offset){
 		var delta = 100 - (parallaxRelativity(parallax) * velocity * 100 - offset * 100);
 		parallax.css("background-position", "25% " +  delta + "%");
 	}
 	
+	/* parallaxRelativity
+	 *	Params
+	 *		parallax	DOM object meant to be considered
+	 *
+	 * will calculate how much the screen has consumed the parallax
+	 * element. in percentage. Anything thing beyond will be accounted 
+	 * for as 0%
+	 */
 	function parallaxRelativity(parallax){
 		var windowTop = $(window).scrollTop();
 		var windowHeight = $(window).height();
@@ -24,6 +48,7 @@ $(document).ready(function() {
 		return 0;
 	}
 
+	/* Make sure that the page will always update via scrolling or window resizing */
 	$(window).bind('scroll', function() { scrollUpdates() }); 
 	$(window).resize( function() { scrollUpdates() });
 });
